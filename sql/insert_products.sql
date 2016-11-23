99f3be411fa1f1ec1071438d7a1f4702ff07e444
Colin <Colin>
2016-11-23 12:15:30 -0800

    Added product insertion sql. Renamed folder.

---
+++ sql/insert_products.sql
@@ -0,0 +1,10 @@
+INSERT INTO Product(cost,pname,description,image) VALUES (45.00,"CN TOWER Vinyl Album","Includes the hit single 'George Bush' and many other top tracks!","album_record2_300.png");
+INSERT INTO Product(cost,pname,description,image) VALUES (10.00,"Snapback Hat","Comes in grey. A fashionable wear for any occasion. Wear it backwards or forwards!","hat.png");
+INSERT INTO Product(cost,pname,description,image) VALUES (25.00,"Hoodie - Grey","Comes in grey. Warm %100 cotton. Stay toasty this winter. Want to look like a total park rat while you shred the pow at Biggie? Buy this.","hoodie1.png");
+INSERT INTO Product(cost,pname,description,image) VALUES (25.00,"Hoodie - Red","Comes in red. Warm %100 cotton. Stay toasty this winter. Want to look like a total park rat while you shred the pow at Biggie? Buy this.","hoodie2.png");
+INSERT INTO Product(cost,pname,description,image) VALUES (5.00,"Lighter - White","Comes in white. Designed in the Smokanagan, this will help you stay lit fam. Anyone spare some darts?","lighter1.png");
+INSERT INTO Product(cost,pname,description,image) VALUES (5.00,"Lighter - Black","Comes in black. Designed in the Smokanagan, this will help you stay lit fam. Anyone spare some darts?","lighter2.png");
+INSERT INTO Product(cost,pname,description,image) VALUES (30.00,"Poster","30cm x 28cm paper poster. Place on your ceiling so you can lay in bed and watch as the stars of his eyes glitter in the night.","poster_300.png");
+INSERT INTO Product(cost,pname,description,image) VALUES (250.00,"Record Player","Very high quality! Each record player has been quality tested by 2Kyle himself. The case is construcyed of reinforced steel. Weight: 50kg. No embedded speakers, has AUX output.","record_player.png");
+
+DELETE FROM Product;
