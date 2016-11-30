b8c26bbe32203321cb109509ef87bc4e03cd1e63
Colin <Colin>
2016-11-29 20:13:09 -0800

    built admin page

---
+++ data/insert_orders.sql
@@ -0,0 +1,14 @@
+delete from CustomerOrder;
+
+INSERT INTO CustomerOrder(province, cid, cartTotal, shippingType) VALUES ("BC",12,67.00,"Express");
+INSERT INTO CustomerOrder(province, cid, cartTotal, shippingType) VALUES ("AB",5,500.00,"Express");
+INSERT INTO CustomerOrder(province, cid, cartTotal, shippingType) VALUES ("SK",9,22.00,"Regular");
+INSERT INTO CustomerOrder(province, cid, cartTotal, shippingType) VALUES ("SK",9,89.00,"Express");
+INSERT INTO CustomerOrder(province, cid, cartTotal, shippingType) VALUES ("SK",10,120.00,"Regular");
+INSERT INTO CustomerOrder(province, cid, cartTotal, shippingType) VALUES ("BC",12,33.00,"Express");
+INSERT INTO CustomerOrder(province, cid, cartTotal, shippingType) VALUES ("AB",11,30.00,"Express");
+INSERT INTO CustomerOrder(province, cid, cartTotal, shippingType) VALUES ("SK",10,11.00,"Regular");
+INSERT INTO CustomerOrder(province, cid, cartTotal, shippingType) VALUES ("BC",8,6.00,"Express");
+INSERT INTO CustomerOrder(province, cid, cartTotal, shippingType) VALUES ("BC",12,45.00,"Express");
+INSERT INTO CustomerOrder(province, cid, cartTotal, shippingType) VALUES ("BC",8,22.00,"Regular");
+
